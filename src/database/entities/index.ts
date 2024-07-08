d790037d9156a4e2289fdea7c59ba7b1ea93db05
Cong Minh <ndmc.system176@gmail.com>
2024-07-08 21:47:19 +0700

    init folder auth

--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -1,1 +1,8 @@
 export * from './role.entity';
+export * from './admin.entity';
+export * from './user.entity';
+// export * from './admin.entity';
+// export * from './admin.entity';
+// export * from './admin.entity';
+// export * from './admin.entity';
+// export * from './admin.entity';
