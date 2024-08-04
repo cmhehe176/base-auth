e316ac34921b7f49a253ebc5eda9b4a89a970f0c
Cong Minh <ndmc.system176@gmail.com>
2024-08-04 19:24:20 +0700

    fix: local strategy

--- src/database/entities/base.entities.ts
+++ src/database/entities/base.entities.ts
@@ -1,13 +1,38 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { UserEntity } from 'src/database/entities/user.entity';
 
-@Entity({ name: 'roles' })
-export class RoleEntity {
+export abstract class BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar' })
-  name: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 
-  @Column({ type: 'varchar', unique: true })
-  alias: string;
+  @Column({ type: 'int', name: 'created_id', nullable: true, select: false })
+  createdId: number | null;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column({ type: 'int', name: 'deleted_id', nullable: true, select: false })
+  deletedId: number | null;
+
+  @DeleteDateColumn({ name: 'deleted_at', select: false })
+  deletedAt: Date;
+
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn({ name: 'created_id' })
+  createdBy?: UserEntity;
+
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn({ name: 'deleted_id' })
+  deletedBy?: UserEntity;
 }
